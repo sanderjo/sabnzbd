3b7a4fb8f90c402791c123791a7a333506ba7a50
Sander Jonkers <sander.jonkers+github@gmail.com>
2021-01-01 08:39:32 +0100

    Let get_from_url() do the work

--- sabnzbd/rss.py
+++ sabnzbd/rss.py
@@ -32,7 +32,7 @@ from sabnzbd.constants import RSS_FILE_NAME, DEFAULT_PRIORITY, DUP_PRIORITY
 from sabnzbd.decorators import synchronized
 import sabnzbd.config as config
 import sabnzbd.cfg as cfg
-from sabnzbd.misc import cat_convert, wildcard_to_re, cat_to_opts, match_str, from_units, int_conv, get_base_url
+from sabnzbd.misc import cat_convert, wildcard_to_re, cat_to_opts, match_str, from_units, int_conv, get_base_url, get_from_url
 import sabnzbd.emailer as emailer
 
 import feedparser
@@ -207,10 +207,7 @@ class RSSReader:
                 uri = uri.replace(" ", "%20").replace("feed://", "http://")
                 logging.debug("Running feedparser on %s", uri)
                 try:
-                    req = urllib.request.Request(uri)
-                    req.add_header("User-Agent", "SABnzbd/%s" % sabnzbd.__version__)
-                    with urllib.request.urlopen(req, timeout=10) as response:
-                        feed_parsed = feedparser.parse(response.read())
+                    feed_parsed = feedparser.parse(get_from_url(uri))
                 except Exception as feedparser_exc:
                     # Feedparser 5 would catch all errors, while 6 just throws them back at us
                     feed_parsed["bozo_exception"] = feedparser_exc
